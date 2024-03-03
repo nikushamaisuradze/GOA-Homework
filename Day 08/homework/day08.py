af5507867263b6874b2e6baaed68f19ccd350e1d
nikushamaisuradze <imtryna5@gmail.com>
2024-03-03 22:56:38 +0400

    commit

---
+++ Day 08/homework/day08.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name: ")
+password = input("Please enter your password: ")
+repeat_password = input("Please enter your password again: ")
+
+if 'nikusha012' == 'nikusha012':
+    print(name, "registered succesfully")
+else:
+    print("invalid password")
---
+++ Day 09/classwork/day9.py
@@ -0,0 +1,5 @@
+# num = int(input('Enter the number: '))
+# print(num > 1 and num < 10)
+
+number = int(input('Enter the number: '))
+print(number % 5 == 0 and number % 10 == 0)
