1011c9285ca031934e97822ece4da11fc884c71d
nikushamaisuradze <imtryna5@gmail.com>
2024-03-17 21:27:00 +0400

    commit

--- Day 12/homework/homewrok.py
+++ Day 12/homework/homewrok.py
@@ -1,18 +1,23 @@
-info = []
+# info = []
 
-name = input("Please enter your name:")
-lastname = input("Please enter your last name: ")
-age = int(input("Please enter your age: "))
-hometown = input("Please enter your home town: ")
+# name = input("Please enter your name:")
+# lastname = input("Please enter your last name: ")
+# age = int(input("Please enter your age: "))
+# hometown = input("Please enter your home town: ")
 
-info.append(name)
-info.append(lastname)
-info.append(age)
-info.append(hometown)
+# info.append(name)
+# info.append(lastname)
+# info.append(age)
+# info.append(hometown)
 
-print(info)
-print(info[0:2])
-print(info[1:3])
-print(info[0:3])
-print(info[1:])
+# print(info)
+# print(info[0:2])
+# print(info[1:3])
+# print(info[0:3])
+# print(info[1:])
+
+
+info = "Nikoloz Maisuradze"
+print(info[0:7])
+
 
